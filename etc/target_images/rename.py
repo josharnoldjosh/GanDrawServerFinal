78448b831f7f85544d12952fd870f87c40282143
JOSH ARNOLD <jarnold@ucdavis.edu>
2020-02-28 13:45:05 -0800

    added start from option

--- etc/target_images/rename.py
+++ etc/target_images/rename.py
@@ -1,4 +1,6 @@
 import os
 
+start_from = input("Start from: ")
+
 for idx, image in enumerate([x for x in os.listdir(os.getcwd()) if ".jpg" in x]):
-    os.rename(image, "target_"+str(idx)+".jpg")
+    os.rename(image, "target_"+str(idx+int(start_from))+".jpg")
